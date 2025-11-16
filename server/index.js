3350eb57e3e9ed96405b61479d81026fa4110a3e
Chaitanya <chaitnayabhagat666@gmail.com>
2025-11-16 20:40:57 +0530

    Updated Readme

--- server/index.js
+++ server/index.js
@@ -17,7 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/campus_navigation')
+console.log(process.env.MONGODB_URI );
+mongoose.connect(process.env.MONGODB_URI )
 .then(() => console.log('MongoDB connected successfully'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
